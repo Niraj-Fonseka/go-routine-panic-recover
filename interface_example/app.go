4a0bda773c0ac9b0b9922e19630a1c4557dc261b
Niraj Fonseka <fonseka.live@gmail.com>
2023-02-12 07:57:40 -0600

    removing type switch (#2)

--- interface_example/app.go
+++ interface_example/app.go
@@ -18,15 +18,8 @@ func main() {
 
 	for w := range wrks {
 		fmt.Printf("\033[31m---------------- PANIC happened in worker : \033[0m\033[34m%s\033[0m\033[31m because %s\033[0m\n", w.GetWorkerID(), w.GetError().Error())
-		switch w.(type) {
 
-		case *workers.HardWorker:
-			fmt.Printf("\033[32m-------------\033[0m \033[34m%s\033[0m \033[32mrecovering ...\033[0m \n", w.GetWorkerID())
-			go w.(*workers.HardWorker).Work(wrks)
-		case *workers.SlowWorker:
-			fmt.Printf("\033[32m-------------\033[0m \033[34m%s\033[0m \033[32mrecovering ...\033[0m \n", w.GetWorkerID())
-			go w.(*workers.SlowWorker).Work(wrks)
-		}
+		go w.Work(wrks)
 
 	}
 
